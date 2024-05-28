faece60a674c0180e48ced1ecf7669ef8406c9b0
Serra Keskin <sekeskin@k2m16s01.42kocaeli.com.tr>
2024-05-28 12:32:33 +0300

    added

---
+++ get_next_line_utils.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sekeskin <sekeskin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 12:54:16 by sekeskin          #+#    #+#             */
+/*   Updated: 2024/01/26 21:11:33 by sekeskin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
+
+	dup = (char *)malloc(ft_strlen(s) + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (*s)
+		dup[i++] = *s++;
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*new_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		new_str[i++] = *s1++;
+	while (*s2)
+		new_str[i++] = *s2++;
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+	size_t	i;
+	size_t	j;
+	size_t	calc_len;
+
+	calc_len = ft_strlen(s);
+	if (!s || len <= 0)
+		return (NULL);
+	if (len > calc_len)
+		len = calc_len;
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	i = start;
+	j = 0;
+	while (i < calc_len && j < len)
+	{
+		res[j] = s[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+	return (res);
+}
+
+void	*ft_free_stash(char **stash, int create_line)
+{
+	char	*line;
+
+	if (!*stash)
+		return (NULL);
+	if (create_line == 0)
+	{
+		if (*stash)
+		{
+			free(*stash);
+			*stash = NULL;
+		}
+		return (NULL);
+	}
+	else if (create_line == 1)
+	{
+		line = ft_strdup(*stash);
+		free(*stash);
+		*stash = NULL;
+		return (line);
+	}
+	return (NULL);
+}
